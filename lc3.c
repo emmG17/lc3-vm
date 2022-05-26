ff48779f6259a1b543358ce49eebfe254f003fcc
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-05-25 20:59:48 -0500

    Add main function scaffold

--- lc3.c
+++ lc3.c
@@ -43,3 +43,70 @@ enum {
 uint16_t memory[MEMORY_MAX];
 
 uint16_t reg[R_COUNT];
+
+int main(int argc, const char* argv[])
+{
+	if (argc < 2)
+	{
+		printf("lc3 [image-file1] ...\n");
+		exit(2);
+	}
+
+	for (int j = 1; j < argc; ++j)
+	{
+		if (!read_image(argv[j]))
+		{
+			printf("failed to load image: %s\n", argv[j]);
+			exit(1);
+		}
+	}
+	/* TODO initial setup */
+
+	reg[R_COND] = FL_ZRO;
+
+	enum { PC_START = 0x3000 };
+	reg[R_PC] = PC_START;
+
+	int running = 1;
+	while (running)
+	{
+		uint16_t instruction = mem_read(reg[R_PC]);
+		uint16_t opcode = instruction << 12;
+
+		switch (op)
+		{
+			case OP_ADD:
+				break;
+			case OP_AND:
+				break;
+			case OP_NOT:
+				break;
+			case OP_BR:
+				break;
+			case OP_JMP:
+				break;
+			case OP_JSR:
+				break;
+			case OP_LD:
+				break;
+			case OP_LDI:
+				break;
+			case OP_LDR:
+				break;
+			case OP_LEA:
+				break;
+			case OP_ST:
+				break;
+			case OP_STI:
+				break;
+			case OP_TRAP:
+				break;
+			case OP_RES:
+			case OP_RTI:
+			default:
+				/*BAD CODE*/
+				break;
+		}
+	}
+	/*shutdown*/
+}
