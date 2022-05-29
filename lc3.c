8d0da14a9f0a943c7e877e4fb695815eb5c0cd70
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-05-28 23:42:57 -0500

    Add functions to read a program file

--- lc3.c
+++ lc3.c
@@ -78,6 +78,37 @@ void update_flags(uint16_t r)
 
 }
 
+void read_image_file (FILE* file)
+{
+	uint16_t origin;
+	fread(&origin, sizeof(origin), 1, file);
+	origin = swap16(origin);
+
+	uint16_t max_read = MEMORY_MAX - origin;
+	uint16_t* p = memory + origin;
+	size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+	while (read-- > 0)
+	{
+		*p = swap16(*p);
+		++p;
+	}
+}
+
+uint16_t swap16(uint16_t x)
+{
+	return (x << 8) | (x >> 8);
+}
+
+int read_image(const char* image_path)
+{
+	FILE* file = fopen(image_path, "rb");
+	if (!file) { return 0 };
+	read_image_file(file);
+	fclose(file);
+	return 1;
+}
+
 int main(int argc, const char* argv[])
 {
 	if (argc < 2)
@@ -276,7 +307,7 @@ int main(int argc, const char* argv[])
 						}
 						fflush(stdout);
 						break;
-						
+
 					case TRAP_HALT:
 						puts("HALT");
 						fflush(stdout);
